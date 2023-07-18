509005e2c706cdbd3543fd0bf190b6b4e5862c46
AlecJDavidson <alecjdavidson@outlook.com>
2023-07-18 09:20:22 -0400

    Good stopping point for now.

--- packages/webui/src/components/ui/PartSearch.tsx
+++ packages/webui/src/components/ui/PartSearch.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { useLazyQuery } from '@apollo/client'
+import React, { useState, useEffect } from 'react';
+import { useLazyQuery } from '@apollo/client';
 import {
   Box,
   Center,
@@ -12,41 +12,50 @@ import {
   Th,
   Thead,
   Tr,
-} from '@chakra-ui/react'
-import { SearchIcon } from '@chakra-ui/icons'
+  IconButton,
+} from '@chakra-ui/react';
+import { SearchIcon, AddIcon, MinusIcon } from '@chakra-ui/icons';
 
-import { SEARCH_PARTS } from '../../graphql/queries/partQueries'
-import { Part } from '../../types/Part'
+import { SEARCH_PARTS } from '../../graphql/queries/partQueries';
+import { Part } from '../../types/Part';
 
 const PartsSearch: React.FC = () => {
-  const [searchQuery, setSearchQuery] = useState('')
-  const [searchParts, { loading, data }] = useLazyQuery<{ partsBy: Part[] }>(
-    SEARCH_PARTS,
-  )
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchParts, { loading, data }] = useLazyQuery<{ partsBy: Part[] }>(SEARCH_PARTS);
 
   useEffect(() => {
     // Trigger the search query whenever the searchQuery state changes
-    searchParts({ variables: { search: searchQuery } })
-  }, [searchQuery, searchParts])
+    searchParts({ variables: { search: searchQuery } });
+  }, [searchQuery, searchParts]);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(event.target.value)
-  }
+    setSearchQuery(event.target.value);
+  };
+
+  const handleIncreaseQuantity = (partId: string) => {
+    // Your mutation logic to increase the quantity here
+    // For example: call a mutation function from Apollo Client or your preferred state management library
+  };
+
+  const handleDecreaseQuantity = (partId: string) => {
+    // Your mutation logic to decrease the quantity here
+    // For example: call a mutation function from Apollo Client or your preferred state management library
+  };
 
   return (
     <Center>
       <Box border="1px solid #ccc" p={4} borderRadius="md">
         <InputGroup mt={4}>
           <Input
-            placeholder='Search for parts...'
+            placeholder="Search for parts..."
             value={searchQuery}
             onChange={handleInputChange}
           />
           <InputRightElement>
-            <SearchIcon color='gray.300' cursor='pointer' />
+            <SearchIcon color="gray.300" cursor="pointer" />
           </InputRightElement>
         </InputGroup>
-        <Table variant='striped' size='md' mt={4}>
+        <Table variant="striped" size="md" mt={4}>
           <Thead>
             <Tr>
               <Th>Name</Th>
@@ -75,7 +84,21 @@ const PartsSearch: React.FC = () => {
                   <Td>{part.bin}</Td>
                   <Td>{part.container}</Td>
                   <Td>{part.location}</Td>
-                  <Td>{part.quantity}</Td>
+                  <Td>
+                    <IconButton
+                      size="sm"
+                      aria-label="Decrease Quantity"
+                      icon={<MinusIcon />}
+                      onClick={() => handleDecreaseQuantity(part.id)}
+                    />
+                    {part.quantity}
+                    <IconButton
+                      size="sm"
+                      aria-label="Increase Quantity"
+                      icon={<AddIcon />}
+                      onClick={() => handleIncreaseQuantity(part.id)}
+                    />
+                  </Td>
                   <Td>{part.tags.join(', ')}</Td>
                 </Tr>
               ))
@@ -84,8 +107,7 @@ const PartsSearch: React.FC = () => {
         </Table>
       </Box>
     </Center>
-  )
-}
+  );
+};
 
-export default PartsSearch
-
+export default PartsSearch;
