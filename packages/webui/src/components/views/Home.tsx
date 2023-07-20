99eb1883ab5c795641f7a13a07ac21aa25d26602
AlecJDavidson <alecjdavidson@outlook.com>
2023-07-20 13:35:40 -0400

    Started work on the docker setup

--- packages/webui/src/components/views/Home.tsx
+++ packages/webui/src/components/views/Home.tsx
@@ -1,19 +1,38 @@
-import React from 'react'
-import { Flex, HStack, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Flex, HStack, VStack, IconButton } from '@chakra-ui/react'
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons'
 import AddPartForm from '../ui/forms/AddPart'
 import PartsSearch from '../ui/PartSearch'
 
 const Home: React.FC = () => {
+  const collapsed: boolean = false 
+  const [showAddPartForm, setShowAddPartForm] = useState(collapsed)
+
+  const handleToggleForm = () => {
+    setShowAddPartForm((prevState) => !prevState)
+  }
+
   return (
     <Flex direction='column' mt={8}>
-      <HStack mt={4} alignItems="flex-start">
-        <VStack>
-          <AddPartForm />
-        </VStack>
+      <HStack mt={4} alignItems='flex-start'>
+        <Flex alignItems='stretch' flex={showAddPartForm ? 1 : 0}>
+          {showAddPartForm && (
+            <VStack>
+              <AddPartForm />
+            </VStack>
+          )}
+        </Flex>
         <VStack>
           <PartsSearch />
         </VStack>
       </HStack>
+      <Flex alignItems='center' mt={4} ml={4}>
+        <IconButton
+          aria-label={showAddPartForm ? 'Collapse Form' : 'Expand Form'}
+          icon={showAddPartForm ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          onClick={handleToggleForm}
+        />
+      </Flex>
     </Flex>
   )
 }
