c76fab06f18e9cace8bf5c85fed9a928d92991af
AlecJDavidson <alecjdavidson@outlook.com>
2023-07-20 18:33:27 -0400

    Using a bash script to start everything quickly until I get the docker stuff fixed.

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Function to start Docker Compose
+start_docker_compose() {
+  echo "Starting Docker Compose..."
+  docker-compose up -d --build
+}
+
+# Function to run yarn start for webui
+start_webui() {
+  echo "Starting webui..."
+  yarn webui:start
+}
+
+# Function to run yarn start for server
+start_server() {
+  echo "Starting server..."
+  yarn server:start
+}
+
+# Main script execution
+start_docker_compose
+start_webui &
+start_server
