8e5823c55902898503792517386d648ff2904af5
AlecJDavidson <alecjdavidson@outlook.com>
2025-01-11 12:57:42 -0500

    Added startup script to control the startup of the db over the server and webui. Going to solve this with docker at some point. Tested and working now.

--- packages/webui/vite.config.ts
+++ packages/webui/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3051 
+  },
   plugins: [react()],
   build: {
     rollupOptions: {
